144d25a4cbb46c8e748f9849cb868194658f9402
tintin0013 <tintin0013@gmail.com>
2022-05-23 18:11:56 +0200

    mise en place du fetch pour la page index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,42 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
+    console.log(data);
+    const { name, portrait, city, country, id, tagline, price } = data;
+    const picture = `assets/photographers/portrait/${portrait}`;
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+
+        const link = document.createElement('a');
+        link.setAttribute('class','photographer_link');
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.className= "photographer_photo"
+        img.setAttribute('alt', `Aperçu profil de ${name}`)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        link.appendChild(img)
+        link.appendChild(h2)
+        // ici je crée un element paragraphe, dans le quel je vais y inseré les données correspondante grace a innerHTML, je rajoute la virgule en concaténant les resultat.
+        const pays = document.createElement('p')
+        // je rajoute une class au paragraphe pour pouvoir mieux le reconnaitre.
+        pays.className = 'photographer_villePays'
+
+        pays.innerHTML = country+", "+city;
+   
+        const tag = document.createElement('p');
+        tag.className = "photographer_tag"
+        tag.innerHTML = tagline;
+        const prix = document.createElement('p')
+        prix.className = "photographer_prix"
+        prix.innerHTML = price+'€/par heure'
+        
+        article.appendChild(link)
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pays)
+        article.appendChild(tag)
+        article.appendChild(prix)
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, id, tagline, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,35 +1,20 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
-
+        
+        const response = await fetch('./data/photographers.json',{
+            method: 'GET'
+        })
+        .then(response =>response.text())
+        let json = JSON.parse(response);
+        return json
+        }
+     
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
-
+        console.log(photographers   );
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
+            console.log(photographerModel);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
@@ -37,9 +22,9 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        const  photographers  = await getPhotographers()
+        displayData(photographers.photographers);
     };
-    
+    // console.log(init());
     init();
     
